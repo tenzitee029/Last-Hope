b0c23cbf611d96a8823f4ff17c5bc14b664da24f
Nguyễn Nhật Tiến <bryantien2405@gmail.com>
2026-09-08 17:46:21 +0700

    Chuyển động nhân vật

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer ()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.linearVelocity = playerInput.normalized * moveSpeed;
+        if (playerInput.x<0)
+        {
+            spriteRenderer.flipX = true;
+        }else if (playerInput.x>0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        if (playerInput != Vector2.zero)
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+    }
+}
